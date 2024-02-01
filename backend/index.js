b3ed4d1715d35e32c138449398fed74792d9b5f0
Shikhar Bansal <shikharbansal007@gmail.com>
2024-02-01 16:07:24 +0530

    bug fix

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const cors = require("cors");
-
-app.use(cors());
+const app = express();
 app.use(express.json());
+app.use(cors());
 
 const rootRouter = require("./routes/index");
-
-const app = express();
 
 app.use("/api/v1", rootRouter);
 app.listen(3000);
