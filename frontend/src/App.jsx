08d5df4e090f801eeb81fd49d7b4fb2d9eeb700c
Shikhar Bansal <shikharbansal007@gmail.com>
2024-02-14 20:34:13 +0530

    Tailwind and Routes setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom"
 
 function App() {
 
   return (
-    <div>
-        Hello world
-    </div>
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element = {<Signup/>} />
+        <Route path="/signin" element = {<Signin/>} />
+        <Route path="/Dashboard" element = {<Dashboard/>} />
+        <Route path="/Sendmoney" element = {<SendMoney/>} />
+
+      </Routes>
+    </BrowserRouter>
+    
+    </>
   )
 }
 
